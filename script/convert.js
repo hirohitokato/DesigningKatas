4235eb860b4b61b33822025b133a1f3c954b844f
Kato Hirohito <Kato.Hirohito@exc.epson.co.jp>
2024-12-16 09:07:02 +0900

    updates contents

--- script/convert.js
+++ script/convert.js
@@ -19,6 +19,9 @@ const outputFilePath = path.join(outputDir, process.argv[3]);
 // Markdown-itのインスタンスを作成
 const md = new markdownIt();
 
+// HTMLのテンプレートを読み込む
+const template = fs.readFileSync('script/template.html', 'utf-8');
+
 // README.mdを読み込む
 const markdown = fs.readFileSync(inputFilePath, 'utf8');
 
@@ -29,7 +32,10 @@ let updatedMarkdown = markdown.replace(/\.md/g, '.html');
 updatedMarkdown = updatedMarkdown.replace(/<!--.*?-->/gs, '');
 
 // MarkdownをHTMLに変換
-const result = md.render(updatedMarkdown);
+const htmlbody = md.render(updatedMarkdown);
+
+// テンプレートに変換したHTMLを埋め込む
+const result = template.replace('<!-- CONTENT WILL BE INSERTED HERE -->', htmlbody);
 
 // 出力ディレクトリが存在しない場合は作成
 if (!fs.existsSync(outputDir)) {
